5740f5267f4d5cda3ae3de8f88943d77f8f5d079
miguecarrera <ahola6479@gmail.com>
2022-01-11 12:44:50 +0100

    ejercicio mas sencillo de python

--- 02.py
+++ 02.py
@@ -7,14 +7,7 @@ devolver s,e,p,a,r,a,r
 >>> cad = "En un lugar de la Mancha"
 >>> separar(cad)
 'E,n, ,u,n, ,l,u,g,a,r, ,d,e, ,l,a, ,M,a,n,c,h,a'
->>> separar2(cad, 0)
-'En un lugar de la Mancha'
->>> separar2(cad, 24)
-'E,n, ,u,n, ,l,u,g,a,r, ,d,e, ,l,a, ,M,a,n,c,h,a'
->>> separar2(cad, 100)
-'E,n, ,u,n, ,l,u,g,a,r, ,d,e, ,l,a, ,M,a,n,c,h,a'
->>> separar2("separar", 2)
-'s,e,parar'
+
 """
 
 
@@ -24,20 +17,6 @@ def separar(cad):
         salida += letra + ","
     return salida[:-1]
 
-
-def separar2(cad, rmax):
-    if rmax == 0:
-        return cad
-    salida = ""
-    rep = 0
-    for i in range(len(cad)-1):
-        salida = salida + cad[i] + ","
-        rep += 1
-        if rep == rmax:
-            salida = salida + cad[i+1:]
-            return salida
-    salida += cad[len(cad)-1]
-    return salida
 
 
 if __name__ == '__main__':
